7f005203348cd715d9b9d82b5ea6516c75555713
Jarod Sneed <jarodsneed@Jarods-MacBook.local>
2019-06-08 14:58:22 -0400

    adjusted assets again

--- src/index.js
+++ src/index.js
@@ -1,32 +1,69 @@
 import Phaser from "phaser";
-import logoImg from "./assets/logo.png";
 
 const config = {
   type: Phaser.AUTO,
   parent: "phaser-example",
-  width: 800,
-  height: 600,
+  width: 1200,
+  height: 640,
   scene: {
     preload: preload,
-    create: create
+    create: create,
+    update: update
   }
 };
 
 const game = new Phaser.Game(config);
 
 function preload() {
-  this.load.image("logo", logoImg);
+
+  this.load.tilemapTiledJSON('tilemap', 'assets/tilemap.json');
+  this.load.image('tileset', 'assets/tileset.png');
+  this.load.image('bg', 'assets/background.png');
+  this.load.image('fg', 'assets/foreground.png');
+  this.load.atlas('masterNinja', 'assets/Atlas/masterNinja.png', 'assets/Atlas/masterNinja.json');
+
 }
 
 function create() {
-  const logo = this.add.image(400, 150, "logo");
 
-  this.tweens.add({
-    targets: logo,
-    y: 450,
-    duration: 2000,
-    ease: "Power2",
-    yoyo: true,
-    loop: -1
-  });
+  //Create Background
+  this.background = this.add.tileSprite(this.game.config.width/2, this.game.config.height/2, 1200, 640, 'bg');
+  this.foreground = this.add.tileSprite(this.game.config.width/2, this.game.config.height/2, 1200, 640, 'fg');
+  
+  //Create Map & Layer
+  var map = this.make.tilemap({key: 'tilemap'});
+  var tileset = map.addTilesetImage('tileset');
+  var layer = map.createStaticLayer(0, tileset, 0, 0)
+  layer.setCollisionByExclusion([-1]);
+
+  //Set Boundaries
+  this.physics.world.bounds.width = layer.width;
+  this.physics.world.bounds.height = layer.height;
+
+  //Instantiate Player
+  var player = this.add.sprite(16, 576, 'masterNinja', 'assets/Atlas/images/ninja40.png');
+  console.log(player);
+
+   //Add Player Animations
+  var idleFrames = this.anims.generateFrameNames('masterNinja', { start: 40, end: 43, zeroPad: 2, prefix: 'images/ninja', suffix: '.png' });
+  this.anims.create({ key: 'idle', frames: idleFrames, frameRate: 10, repeat: -1 });
+  player.anims.play('idle');
+
+  this.keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+  this.keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+  this.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+  this.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+  this.keyUp = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+  this.keyDown = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
+  this.keyLeft = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+  this.keyRight = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+  this.keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+
+}
+
+function update() {
+
+  this.background.tilePositionX += 2;
+  this.foreground.tilePositionX += 4;
+
 }
